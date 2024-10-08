d0a16b0b7738eaa8e0e75c596125f3a057b5603f
EisBits <a.chypre@gmail.com>
2024-10-08 11:08:27 +0200

    Update doctoresa.sh

--- doctoresa.sh
+++ doctoresa.sh
@@ -26,7 +26,7 @@ LOCKFILE="/tmp/last_email_sent.lock"
 PAGE_CONTENT=$(curl -A "Mozilla/5.0 (Linux; Android 10; SM-G996U Build/QP1A.190711.020; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Mobile Safari/537.36" -s $URL)
 
 if echo "$PAGE_CONTENT" | grep -q "$SEARCH_TEXT"; then
-    echo "The text '$SEARCH_TEXT' was found. No email will be sent."
+    echo "Texte '$SEARCH_TEXT' trouvé. Pas d'envoi de mail."
 else
     if [ -f "$LOCKFILE" ]; then
         LAST_SENT_TIME=$(cat "$LOCKFILE")
